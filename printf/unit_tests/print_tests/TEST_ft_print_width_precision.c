6cf2f0dc857e9ceb6a9b68bca32cb04bc7c26860
Rodrigo Moreira <r.moreira4297@gmail.com>
2020-09-18 20:36:21 -0300

    ft_print_width_precision print_test OK!

--- printf/unit_tests/print_tests/TEST_ft_print_width_precision.c
+++ printf/unit_tests/print_tests/TEST_ft_print_width_precision.c
@@ -1,39 +1,130 @@
-#include "../includes/ft_printf.h"
+#include "../../includes/ft_printf.h"
 #include <stdio.h>
 
-//This function return the padding number instead of printing the padding on the screen
-static int	ft_print_string_width(t_conversion tools, int len)
+static void	TEST_ft_print_width()
 {
-	int		pad_len;
-	int		padding_count = 0; //The value of this variable must be equal to the expected padding valuet.
-
-	pad_len = tools.opts.width - len;
-	while (pad_len--)
-		padding_count++; //ft_putchar_fd(' ', 1); --> original function line.
-	return (padding_count); //Only for tests
-}
-static int	TEST_ft_print_string_width()
-{
+	int				strlen;
 	t_conversion	tools;
 
+	tools.conv = 'c';
+	tools.opts.width = 5;
+	strlen = 1;
+	printf("---- Line = %d -> Print string width test ----\n", __LINE__);
+	printf("    |\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 's';
 	tools.opts.width = 12;
-	if
-	(
-		ft_print_string_width(tools, 4) == 8 &&
-		ft_print_string_width(tools, 1) == 11 &&
-		ft_print_string_width(tools, 11) == 1
-	)
-		return (1);
-	return (0);
+	strlen = 5;
+	printf("\n---- Line = %d -> Print string width test ----\n", __LINE__);
+	printf("       |\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'd';
+	tools.opts.width = 7;
+	strlen = 12;
+	printf("\n---- Line = %d -> Print number width test (len > width > precision) ----\n", __LINE__);
+	printf("|\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'i';
+	tools.opts.width = 7;
+	tools.opts.precision = 10;
+	strlen = 4;
+	printf("\n---- Line = %d -> Print number width test (precision > width > len) ----\n", __LINE__);
+	printf("|\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'u';
+	tools.opts.width = 13;
+	tools.opts.precision = 7;
+	strlen = 4;
+	printf("\n---- Line = %d -> Print number width test (width > precision > len) ----\n", __LINE__);
+	printf("      |\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'p';
+	tools.opts.width = 13;
+	tools.opts.precision = 7;
+	strlen = 4;
+	printf("\n---- Line = %d -> Print number width test (width > precision > len and conversion = '%%p') ----\n", __LINE__);
+	printf("    |\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'x';
+	tools.opts.width = 10;
+	tools.opts.precision = 2;
+	strlen = 7;
+	printf("\n---- Line = %d -> Print number width test (width > len > precision) ----\n", __LINE__);
+	printf("   |\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'X';
+	tools.opts.width = 12;
+	tools.opts.precision = -1;
+	tools.flags.zero = 1;
+	tools.flags.minus = 0;
+	strlen = 4;
+	printf("\n---- Line = %d -> Print number width test (width > len, no precision and zero flag) ----\n", __LINE__);
+	printf("00000000|\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'd';
+	tools.opts.width = 12;
+	tools.opts.precision = 4;
+	tools.flags.zero = 1;
+	tools.flags.minus = 0;
+	strlen = 4;
+	printf("\n---- Line = %d -> Print number width test (width > len = precision and zero flag) ----\n", __LINE__);
+	printf("        |\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	tools.conv = 'd';
+	tools.opts.width = 12;
+	tools.opts.precision = -1;
+	tools.flags.zero = 1;
+	tools.flags.minus = 1;
+	strlen = 7;
+	printf("\n---- Line = %d -> Print number width test (width > len, no precision, minus and zero flag) ----\n", __LINE__);
+	printf("     |\n");
+	ft_print_width(tools, strlen);
+	printf("|\n");
+
+	printf("\n");
+}
+
+static void	TEST_ft_print_precision()
+{
+	t_conversion	tools;
+	int				strlen;
+
+	tools.opts.precision = -1;
+	strlen = 7;
+	printf("\n---- Line = %d -> Print number precision test (len > precision) ----\n", __LINE__);
+	printf("|\n");
+	ft_print_precision(tools, strlen);
+	printf("|\n");
+
+	tools.opts.precision = 12;
+	strlen = 7;
+	printf("\n---- Line = %d -> Print number precision test (precision > len) ----\n", __LINE__);
+	printf("00000|\n");
+	ft_print_precision(tools, strlen);
+	printf("|\n");
 }
 
 int			main(void)
 {
-
-	if (TEST_ft_print_string_width() /*&& TEST_ft_print_number_width()*/)
-		printf("ft_print_width --> OK\n");
-	else
-		printf("ft_print_width --> FAILED\n");
-
+	TEST_ft_print_width();
+	TEST_ft_print_precision();
 	return (0);
 }
