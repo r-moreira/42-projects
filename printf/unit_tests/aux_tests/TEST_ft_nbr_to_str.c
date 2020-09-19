3a4f672915e8af4e27cab3337f3be99fc3a7ebce
Rodrigo Moreira <r.moreira4297@gmail.com>
2020-09-18 21:03:05 -0300

    initializing ft_nbr_to_str unit test

---
+++ printf/unit_tests/aux_tests/TEST_ft_nbr_to_str.c
@@ -0,0 +1,16 @@
+#include "../includes/ft_printf.h"
+#include <stdio.h>
+
+static int	TEST_ft_nbr_to_str()
+{
+	return (0);
+}
+
+int			main(void)
+{
+	if (TEST_ft_nbr_to_str())
+		printf("ft_nbr_to_str --> OK\n");
+	else
+		printf("ft_nbr_to_str --> FAILED\n");
+	return (0);
+}
