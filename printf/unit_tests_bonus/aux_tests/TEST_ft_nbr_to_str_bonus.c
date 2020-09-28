ee6f4b99df686de310ec7de2ba1cc2aa0b99a561
Rodrigo Moreira <r.moreira4297@gmail.com>
2020-09-28 20:21:17 -0300

    add test initial structure to the bonus project

---
+++ printf/unit_tests_bonus/aux_tests/TEST_ft_nbr_to_str_bonus.c
@@ -0,0 +1,59 @@
+#include "../includes/ft_printf.h"
+#include <stdio.h>
+
+static int	TEST_ft_nbr_to_str()
+{
+	t_arg_types		arg;
+	t_conversion	tools;
+	char			*expected_str;
+	int				test_result;
+
+	test_result = 0;
+
+	tools.conv = 'd';
+	arg.u_int = -2147483648;
+	expected_str = "-2147483648";
+	if (!ft_strncmp(ft_nbr_to_str(DECIMAL, arg, tools), expected_str, ft_strlen(expected_str)))
+		test_result += 1;
+
+	tools.conv = 'i';
+	arg.u_int = 2147483647;
+	expected_str = "2147483647";
+	if (!ft_strncmp(ft_nbr_to_str(DECIMAL, arg, tools), expected_str, ft_strlen(expected_str)))
+		test_result += 1;
+
+	tools.conv = 'u';
+	arg.u_uint = 4294967295;
+	expected_str = "4294967295";
+	if (!ft_strncmp(ft_nbr_to_str(DECIMAL, arg, tools), expected_str, ft_strlen(expected_str)))
+		test_result += 1;
+
+	tools.conv = 'x';
+	arg.u_uint = 429496;
+	expected_str = "68db8";
+	if (!ft_strncmp(ft_nbr_to_str(LOWER_HEXA, arg, tools), expected_str, ft_strlen(expected_str)))
+		test_result += 1;
+
+	tools.conv = 'X';
+	arg.u_uint = 4294967295;
+	expected_str = "FFFFFFFF";
+	if (!ft_strncmp(ft_nbr_to_str(UPPER_HEXA, arg, tools), expected_str, ft_strlen(expected_str)))
+		test_result += 1;
+
+	tools.conv = 'p';
+	arg.u_luint = 18446744073709551615ul;
+	expected_str = "ffffffffffffffff";
+	if (!ft_strncmp(ft_nbr_to_str(LOWER_HEXA, arg, tools), expected_str, ft_strlen(expected_str)))
+		test_result += 1;
+
+	return (test_result == 6);
+}
+
+int			main(void)
+{
+	if (TEST_ft_nbr_to_str())
+		printf("ft_nbr_to_str --> OK\n");
+	else
+		printf("ft_nbr_to_str --> FAILED\n");
+	return (0);
+}
---
+++ printf/unit_tests_bonus/print_tests/TEST_ft_ptr_funct_character_bonus.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include "../../includes/ft_printf.h"
+
+int			ft_printf(const char *format, ...);
+
+static void	check_return(int ret, int ret42)
+{
+	if (ret == ret42)
+		printf("---- Return OK ----\n");
+	else
+		printf("---- Return error. libc ret = %d, libft ret = %d ----\n", ret, ret42);
+}
+
+static void char_only_conversion_test()
+{
+	int		ret1;
+	int		ret2;
+
+	printf("---- Line = %d -> Print only char conversion test ----\n", __LINE__);
+	ret1 =    printf("This is %c char test!!\n", 'a');
+	ret2 = ft_printf("This is %c char test!!\n", 'a');
+	check_return(ret1, ret2);
+}
+
+static void	char_width_options_test()
+{
+	int		ret1;
+	int		ret2;
+
+	printf("\n---- Line = %d -> Print char with width (digits option) test ----\n", __LINE__);
+	ret1 =    printf("This is |%5c| char test!!\n", 'a');
+	ret2 = ft_printf("This is |%5c| char test!!\n", 'a');
+	check_return(ret1, ret2);
+
+	printf("\n---- Line = %d -> Print char with width = 1 test ----\n", __LINE__);
+	ret1 =    printf("This is |%1c| char test!!\n", 'a');
+	ret2 = ft_printf("This is |%1c| char test!!\n", 'a');
+	check_return(ret1, ret2);
+
+	printf("\n---- Line = %d -> Print char with negative width (digits option) test ----\n", __LINE__);
+	ret1 =    printf("This is |%-5c| char test!!\n", 'a');
+	ret2 = ft_printf("This is |%-5c| char test!!\n", 'a');
+	check_return(ret1, ret2);
+
+	printf("\n---- Line = %d -> Print char with width (star option) test ----\n", __LINE__);
+	ret1 =    printf("This is |%*c| char test!!\n", 5, 'a');
+	ret2 = ft_printf("This is |%*c| char test!!\n", 5, 'a');
+	check_return(ret1, ret2);
+
+	printf("\n---- Line = %d -> Print char with negative width(star option) test ----\n", __LINE__);
+	ret1 =    printf("This is |%*c| char test!!\n", -5, 'a');
+	ret2 = ft_printf("This is |%*c| char test!!\n", -5, 'a');
+	check_return(ret1, ret2);
+
+	printf("\n---- Line = %d -> Print char with flag minus and width (digits option) ----\n", __LINE__);
+	ret1 =    printf("This is |%5c| char test!!\n", 'a');
+	ret2 = ft_printf("This is |%5c| char test!!\n", 'a');
+	check_return(ret1, ret2);
+
+	printf("\n---- Line = %d -> Print char with flag minus and width (star option) test ----\n", __LINE__);
+	ret1 =    printf("This is |%-*c| char test!!\n", 5, 'a');
+	ret2 = ft_printf("This is |%-*c| char test!!\n", 5, 'a');
+	check_return(ret1, ret2);
+
+	printf("\n---- Line = %d -> Print char with minus flag and negative width (star option) test ----\n", __LINE__);
+	ret1 =    printf("This is |%-*c| char test!!\n", -5, 'a');
+	ret2 = ft_printf("This is |%-*c| char test!!\n", -5, 'a');
+	check_return(ret1, ret2);
+}
+
+int		main(void)
+{
+	char_only_conversion_test();
+	char_width_options_test();
+	return (0);
+}
