a35f265d2f7f49d0b23c87978077921d1e31d46a
Rodrigo Moreira <romoreir@c1r1p4.42sp.org.br>
2020-11-03 22:58:58 -0300

    updating tests

--- ft_printf/tests/error_tests.c
+++ ft_printf/tests/error_tests.c
@@ -17,25 +17,57 @@ int			main() {
 
 	printf("\n----RUNNING TESTS----\n\n");
 
-//	ft_printf("|%5%|\n");
-//	ft_printf("|%-5%|\n");
-//	ft_printf("|%05%|\n");
-//	ft_printf("|%-05%|\n");
-//	ret1 =    printf("%5%");
-//	ret2 = ft_printf("%5%");
-//	check_return(ret1, ret2);
+	ret1 =    printf("|%5%|");
+	ret2 = ft_printf("|%5%|");
+	check_return(ret1, ret2);
 
-	ft_printf("|%p|\n", NULL);
-	ft_printf("|%15p|\n", NULL);
-	ft_printf("|%-15p|\n", NULL);
-	ft_printf("|%.p|\n", NULL);
-	ft_printf("|%.0p|\n", NULL);
-	ft_printf("|%.5p|\n", NULL);
-	ft_printf("|%.10p|\n", NULL);
-	ft_printf("|%5.p|\n", NULL);
-	ft_printf("|%12.5p|\n", NULL);
+	ret1 =    printf("|%-5%|");
+	ret2 = ft_printf("|%-5%|\n");
+	check_return(ret1, ret2);
+	
+	ret1 =    printf("|%05%|");
+	ret2 = ft_printf("|%05%|\n");
+	check_return(ret1, ret2);
+	
+	ret1 =    printf("|%-05%|");
+	ret2 = ft_printf("|%-05%|\n");
+	check_return(ret1, ret2);
+	
+	ret1 =    printf("|%p|\n", NULL);
+	ret2 = ft_printf("|%p|\n", NULL);
+	check_return(ret1, ret2);
 
+	ret1 =    printf("|%15p|\n", NULL);
+	ret2 = ft_printf("|%15p|\n", NULL);
+	check_return(ret1, ret2);
 
+	ret1 =    printf("|%-15p|\n", NULL);
+	ret2 = ft_printf("|%-15p|\n", NULL);
+	check_return(ret1, ret2);
+	
+	ret1 =    printf("|%.p|\n", NULL);
+	ret2 = ft_printf("|%.p|\n", NULL);
+	check_return(ret1, ret2);
+
+	ret1 =    printf("|%.0p|\n", NULL);
+	ret2 = ft_printf("|%.0p|\n", NULL);
+	check_return(ret1, ret2);
+
+	ret1 =    printf("|%.5p|\n", NULL);
+	ret2 = ft_printf("|%.5p|\n", NULL);
+	check_return(ret1, ret2);
+
+	ret1 =    printf("|%.10p|\n", NULL);
+	ret2 = ft_printf("|%.10p|\n", NULL);
+	check_return(ret1, ret2);
+
+	ret1 =    printf("|%5.p|\n", NULL);
+	ret2 = ft_printf("|%5.p|\n", NULL);
+	check_return(ret1, ret2);
+
+	ret1 =    printf("|%12.5p|\n", NULL);
+	ret2 = ft_printf("|%12.5p|\n", NULL);
+	check_return(ret1, ret2);
 
 	return (0);
 }
