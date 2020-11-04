d6b2ecb2402c4ce1d329406317bec639bd857cbe
Rodrigo Moreira <r.moreira4297@gmail.com>
2020-11-03 22:15:33 -0300

    adding width to percent conversion

--- ft_printf/srcs/print_functions/ft_ptr_funct_percent.c
+++ ft_printf/srcs/print_functions/ft_ptr_funct_percent.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ptr_funct_percent.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: romoreir <romoreir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: rodrigo <rodrigo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 14:12:11 by romoreir          #+#    #+#             */
-/*   Updated: 2020/09/20 14:12:11 by romoreir         ###   ########.fr       */
+/*   Updated: 2020/11/03 22:11:20 by rodrigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int				ft_ptr_funct_percent(va_list *args, t_conversion tools)
 {
 	(void)args;
-	(void)tools;
+	if (tools.opts.width > 1 && !tools.flags.minus)
+		ft_print_width(tools, 1);
 	ft_putchar_fd('%', 1);
-	return (1);
+	if (tools.opts.width > 1 && tools.flags.minus)
+		ft_print_width(tools, 1);
+	return (tools.opts.width > 1 ? tools.opts.width : 1);
 }
