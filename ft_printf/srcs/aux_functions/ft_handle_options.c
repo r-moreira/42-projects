0238975eb5696b0e351b5fb3694e73b497738d0f
Rodrigo Moreira Avila <r.moreira4297@gmail.com>
2021-06-20 12:16:08 -0300

    Improving code smell

--- ft_printf/srcs/aux_functions/ft_handle_options.c
+++ ft_printf/srcs/aux_functions/ft_handle_options.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_handle_options.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: romoreir <romoreir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 19:42:02 by romoreir          #+#    #+#             */
-/*   Updated: 2020/09/20 19:42:02 by romoreir         ###   ########.fr       */
+/*   Updated: 2021/06/20 11:10:00 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_printf.h"
 
-int		ft_is_option(char c)
+int	t_is_option(char c)
 {
 	if (c == '*')
 		return (E_STAR);
@@ -25,10 +25,10 @@ int		ft_is_option(char c)
 	return (E_FALSE);
 }
 
-int		ft_is_conversion(char c)
+int	ft_is_conversion(char c)
 {
-	if (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' || c == 'u' ||
-		c == 'x' || c == 'X' || c == '%')
+	if (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i'
+		|| c == 'u' || c == 'x' || c == 'X' || c == '%')
 		return (E_CONV);
 	return (E_FALSE);
 }
