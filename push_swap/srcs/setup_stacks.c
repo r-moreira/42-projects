060be9c09883ae8c393d4a31bacadb2e2ce08b8a
Rodrigo Moreira Avila <r.moreira4297@gmail.com>
2021-09-20 22:52:31 -0300

    starting to work with linked lists 2

--- push_swap/srcs/setup_stacks.c
+++ push_swap/srcs/setup_stacks.c
@@ -50,17 +50,22 @@ void	free_a(t_stacks *stacks)
 		free(temp);
    	}
 }
-void	setup_stacks(t_stacks *stacks, int argc, unsigned int *index)
+
+void	put_indexes_into_stack_a(t_stacks *stacks, unsigned int *index)
 {
 	int	 i;
 
-	init_stacks(stacks, argc);
 	i = stacks->len;
 	while (i > 0) {
 		push_a(stacks, index[i - 1]);
 		i--;
 	}
+}
+
+void	setup_stacks(t_stacks *stacks, int argc, unsigned int *index)
+{
+	init_stacks(stacks, argc);
+	put_indexes_into_stack_a(stacks, index);
 	display_a(stacks);
-		i = stacks->len;
 	free_a(stacks);
 }
