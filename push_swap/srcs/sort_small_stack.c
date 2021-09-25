a10f4cdadca60a0149067d1821c4a096e76a604e
Rodrigo Moreira Avila <r.moreira4297@gmail.com>
2021-09-25 16:57:58 -0300

    fixing sorting four

--- push_swap/srcs/sort_small_stack.c
+++ push_swap/srcs/sort_small_stack.c
@@ -6,7 +6,7 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 12:10:16 by romoreir          #+#    #+#             */
-/*   Updated: 2021/09/25 16:26:35 by romoreir         ###   ########.fr       */
+/*   Updated: 2021/09/25 16:53:16 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,50 +23,68 @@ static void	sort_two(t_stacks *stacks)
 		ra(stacks);
 }
 
-static void	sort_three(t_stacks *stacks, int first, int second, int third)
+static void	sort_three_first_less_than_second(t_stacks *stacks,
+int first, int second, int third)
 {
-	if (first < second)
+	if (first < third)
 	{
-		if (first < third) //1 3 2 -> 3 1 2 -> 1 2 3
+		if (second < third)
+			return ;
+		if (second > third)
 		{
 			sa(stacks);
 			ra(stacks);
-		}
-		if (first > third) // 2 3 1 -> 3 2 1 -> 2 1 3 -> 1 2 3
-		{
-			sa(stacks);
-			ra(stacks);
-			sa(stacks);
 		}
 	}
-	else if (first > second)
+	if (first > third)
 	{
-		if (first < third) //2 1 3 -> 1 2 3
-			sa(stacks);
-		if (first > third)
+		sa(stacks);
+		ra(stacks);
+		sa(stacks);
+	}
+}
+
+static void sort_three_first_greater_than_second(t_stacks *stacks,
+int first, int second, int third)
+{
+	if (first < third)
+		sa(stacks);
+	if (first > third)
+	{
+		if (second < third)
+			ra(stacks);
+		if (second > third)
 		{
-			if (second < third) //3 1 2 -> 1 2 3
-				ra(stacks);
-			if (second > third) // 3 2 1
-			{
-				ra(stacks);
-				sa(stacks);
-			}
+			ra(stacks);
+			sa(stacks);
 		}
 	}
 }
 
-static void	sort_four(t_stacks *stacks, int first, int second, int third)
+static void	sort_four(t_stacks *stacks)
 {
+	int	first;
+	int	second;
+	int	third;
+
 	while (stacks->a->index != 0)
 		ra(stacks);
 	pb(stacks);
-	sort_three(stacks, first, second, third);
+	first = stacks->a->index;
+	second = stacks->a->next->index;
+	third = stacks->a->next->next->index;
+	if (first > second)
+		sort_three_first_greater_than_second(stacks, first, second, third);
+	if (first < second)
+		sort_three_first_less_than_second(stacks, first, second, third);
 	pa(stacks);
 }
 
-static void	sort_five(t_stacks *stacks, int first, int second, int third)
+static void	sort_five(t_stacks *stacks)
 {
+	int	first;
+	int	second;
+	int	third;
 	int i;
 
 	i = -1;
@@ -76,7 +94,13 @@ static void	sort_five(t_stacks *stacks, int first, int second, int third)
 		else
 			ra(stacks);
 	}
-	sort_three(stacks, first, second, third);
+	first = stacks->a->index;
+	second = stacks->a->next->index;
+	third = stacks->a->next->next->index;
+	if (first > second)
+		sort_three_first_greater_than_second(stacks, first, second, third);
+	if (first < second)
+		sort_three_first_less_than_second(stacks, first, second, third);
 	pa(stacks);
 	pa(stacks);
 	if (stacks->a->index > stacks->a->next->index)
@@ -95,9 +119,14 @@ void	sort_small_stack(t_stacks *stacks)
 	if (stacks->len == 2)
 		sort_two(stacks);
 	else if (stacks->len == 3)
-		sort_three(stacks, first, second, third);
+	{
+		if (first > second)
+			sort_three_first_greater_than_second(stacks, first, second, third);
+		if (first < second)
+			sort_three_first_less_than_second(stacks, first, second, third);
+	}
 	else if (stacks->len == 4)
-		sort_four(stacks, first, second, third);
+		sort_four(stacks);
 	else if (stacks->len == 5)
-		sort_five(stacks, first, second, third);
+		sort_five(stacks);
 }
