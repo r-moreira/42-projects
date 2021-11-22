b3cfcb2267c40c86387e520400f5e8e106f39a12
r-moreira <r.moreira4297@gmail.com>
2021-11-22 20:56:17 -0300

    FEATURE: Adding minishell loop

--- minishell/includes/minishell.h
+++ minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:45:12 by romoreir          #+#    #+#             */
-/*   Updated: 2021/11/01 11:47:03 by romoreir         ###   ########.fr       */
+/*   Updated: 2021/11/22 20:49:28 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,24 @@
 # define MINISHELL_H
 
 # include "../libft/libft.h"
-# include <stdlib.h>
+# include<stdio.h>
+# include<string.h>
+# include<stdlib.h>
+# include<unistd.h>
+# include<sys/types.h>
+# include<sys/wait.h>
+# include<readline/readline.h>
+# include<readline/history.h>
+
+
+#define MAX_LINE_INPUT 1000
+#define MAX_COMANDS_NUM 100
+#define CLEAR_CLI "\033[H\033[J"
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE
+}			t_bool;
 
 #endif
--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,14 +6,58 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:43:06 by romoreir          #+#    #+#             */
-/*   Updated: 2021/11/01 11:46:39 by romoreir         ###   ########.fr       */
+/*   Updated: 2021/11/22 20:55:37 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+void	init_shell(void)
+{
+	char	*username;
+
+	username = getenv("USER");
+	printf(CLEAR_CLI);
+	printf("\n===============================================\n");
+	printf("\n---- Welcome: @%s!\n", username);
+	printf("\n---- This is romoreir shell\n");
+	printf("\n===============================================\n");
+}
+
+int	take_input(char *str)
+{
+	char	*buf;
+
+	buf = readline(" $> ");
+	if (strlen(buf) != 0)
+	{
+		add_history(buf);
+		ft_strlcpy(str, buf, ft_strlen(buf) + 1);
+		return (FALSE);
+	}
+	else
+		return (TRUE);
+}
+
+void	print_dir(void)
+{
+	char	cwd[1024];
+
+	getcwd(cwd, sizeof(cwd));
+	printf("\n%s", cwd);
+}
+
 int	main(void)
 {
-	ft_putstr_fd("Hello World", STDOUT_FILENO);
+	char	input_string[MAX_LINE_INPUT];
+
+	init_shell();
+	while (TRUE)
+	{
+		print_dir();
+		take_input(input_string);
+		printf("%s\n", input_string);
+
+	}
 	return (EXIT_SUCCESS);
 }
