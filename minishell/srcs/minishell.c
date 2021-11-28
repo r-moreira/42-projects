b31a6ee68dbe155655ca8f495de8b5067db9c5a3
r-moreira <r.moreira4297@gmail.com>
2021-11-28 03:09:53 -0300

    TO-DO: normalize_cmd_token

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 11:43:06 by romoreir          #+#    #+#             */
-/*   Updated: 2021/11/28 01:43:08 by romoreir         ###   ########.fr       */
+/*   Updated: 2021/11/28 03:09:37 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ void	init_shell(t_shell *sh)
 	(void)sh;
 }
 
-char	*command_tokenizer(char *input)
+
+
+char	*cmd_tokenizer(char *input)
 {
 	int		i;
 	char	*token;
@@ -48,24 +50,51 @@ char	*command_tokenizer(char *input)
 	return (token);
 }
 
+////////////TO-DO
+// - Adicionar espaços antes e depois de '>' '>>' '<' '<<' '|' para o split funcionar direito
+char	*normalize_cmd_token(char *cmd_token)
+{
+	return (cmd_token);
+}
+
+void	create_cmds_struct(t_shell *sh, char **cmd_tokens, int len)
+{
+	char	**cmd_token_split;
+	int		i;
+	int		j;
+
+	i = -1;
+	while (++i <= len)
+	{
+		printf("%s\n", cmd_tokens[i]);
+		cmd_token_split = ft_split(normalize_cmd_token(cmd_tokens[i]), ' ');
+		j = -1;
+		while (cmd_token_split[++j])
+			if (ft_strncmp(cmd_token_split[j], "", ft_strlen(cmd_token_split[j])))
+				printf("%s|", cmd_token_split[j]);
+		printf("\n");
+		free(cmd_token_split);
+	}
+}
+
 void	analyzer(t_shell *sh)
 {
-	char	*command_tokens[MAX_COMMANDS_NUM];
+	char	*cmd_tokens[MAX_COMMANDS_NUM];
 	int		pointer_position;
 	int		i;
 
+	i = -1;
 	pointer_position = 0;
-	i = 0;
 	while (pointer_position < ft_strlen(sh->input_string))
 	{
-		command_tokens[i] = command_tokenizer(sh->input_string + pointer_position);
-		pointer_position += ft_strlen(command_tokens[i]);
-		printf("%s\n", command_tokens[i]);
-		i++;
+		cmd_tokens[++i] = cmd_tokenizer(sh->input_string + pointer_position);
+		pointer_position += ft_strlen(cmd_tokens[i]);
 	}
 
-	while (--i > 0)
-		free(command_tokens[i]);
+	create_cmds_struct(sh, cmd_tokens, i);
+
+	while (i-- > 0)
+		free(cmd_tokens[i]);
 }
 
 int	main(void)
