c5d06f77675f186dffc6bd168d7ce4897105ac55
r-moreira <r.moreira4297@gmail.com>
2021-12-31 15:38:07 -0300

    FIX: Heredoc and FEATURE: Exclude Heredoc Input End

--- minishell/srcs/utils/take_input.c
+++ minishell/srcs/utils/take_input.c
@@ -6,45 +6,50 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 21:10:24 by romoreir          #+#    #+#             */
-/*   Updated: 2021/12/30 21:50:57 by romoreir         ###   ########.fr       */
+/*   Updated: 2021/12/31 15:37:00 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static t_bool	is_here_document(char *line_read)
+static t_bool	is_here_document(char *parsed_line)
 {
 	int	i;
 
 	i = 0;
-	while (line_read[++i])
-		if (line_read[i - 1] == '<' && line_read[i] == '<')
+	while (parsed_line[++i])
+		if (parsed_line[i - 1] == '<' && parsed_line[i] == '<')
 			return (TRUE);
 	return (FALSE);
 }
 
-static char	*parse_heredoc_input_ending(char *line_read)
+static char	*parse_heredoc_input_end(char *parsed_line)
 {
 	int		i;
 	int		j;
 	int		len;
-	char	*input_ending;
+	char	*input_end;
 
 	i = 1;
-	while (line_read[i - 1] != '<' && line_read[i] != '<')
+	while (TRUE)
+	{
+		if (parsed_line[i - 1] == '<' && parsed_line[i] == '<')
+			break ;
 		i++;
-	i += 2;
-	len = ft_strlen(line_read);
-	input_ending = (char *)malloc(sizeof(char) * (len - i) + 10);
+	}
+	while(ft_isspace(parsed_line[i]))
+		i++;
+	len = ft_strlen(parsed_line);
+	input_end = (char *)malloc(sizeof(char) * (len - i) + 1);
 	j = -1;
 	while (i++ < len)
-		if (!ft_isspace(line_read[i]))
-			input_ending[++j] = line_read[i];
-	input_ending[++j] = '\0';
-	return (input_ending);
+		if (!ft_isspace(parsed_line[i]))
+			input_end[++j] = parsed_line[i];
+	input_end[++j] = '\0';
+	return (input_end);
 }
 
-static char	*take_heredoc_input(t_shell *sh, char *input_ending)
+static char	*take_heredoc_input(t_shell *sh, char *input_end)
 {
 	char	*heredoc_input;
 	char	*hdoc_line_read;
@@ -57,7 +62,7 @@ static char	*take_heredoc_input(t_shell *sh, char *input_ending)
 		hdoc_line_read = readline("> ");
 		if (hdoc_line_read && *hdoc_line_read)
 		{
-			if (!ft_strncmp(input_ending, hdoc_line_read, sizeof(input_ending)))
+			if (!ft_strncmp(input_end, hdoc_line_read, sizeof(input_end)))
 				break ;
 			tmp_ptr = heredoc_input;
 			heredoc_input = ft_strjoin(
@@ -73,41 +78,48 @@ static char	*take_heredoc_input(t_shell *sh, char *input_ending)
 	return (parsed_heredoc_in);
 }
 
-static t_status	handle_here_document_input(t_shell *sh, char *line_read)
+static t_status	handle_here_document_input(t_shell *sh, char *parsed_line)
 {
-	char	*input_ending;
+	char	*input_end;
+	char	*tmp;
 
-	input_ending = parse_heredoc_input_ending(line_read);
-	if (input_ending[0] == '\0')
+	input_end = parse_heredoc_input_end(parsed_line);
+	if (input_end[0] == '\0')
 		return (syntax_error("Heredoc '<<' doesn't contain an input ending."));
-	sh->heredoc_file_buffer = take_heredoc_input(sh, input_ending);
-	free(input_ending);
+	sh->heredoc_file_buffer = take_heredoc_input(sh, input_end);
+	tmp = ft_remove_substr(parsed_line, input_end, ft_strlen(parsed_line));
+	ft_strlcpy(sh->input_string, tmp, ft_strlen(tmp) + 1);
+	free(parsed_line);
+	free(input_end);
+	free(tmp);
 	return (SUCCESS);
 }
 
-//TO-DO
-//FIX heredoc with "<" FLAG (< and <<)
-//FIX heredoc wh FLAG <<EOF (No spaces between)
 t_status	take_input(t_shell *sh)
 {
 	char	*line_read;
-	char	*parsed_line_read;
+	char	*parsed_line;
 
 	line_read = readline(NULL);
 	if (line_read && *line_read)
 	{
 		add_history(line_read);
-		parsed_line_read = parse_env_variables(line_read);
-		if (is_here_document(parsed_line_read) == TRUE)
-			if (handle_here_document_input(sh, parsed_line_read) == ERROR)
+		parsed_line = parse_env_variables(line_read);
+		if (is_here_document(parsed_line) == TRUE)
+		{
+			if (handle_here_document_input(sh, parsed_line) == ERROR)
 				return (ERROR);
-		ft_strlcpy(sh->input_string, parsed_line_read,
-			ft_strlen(parsed_line_read) + 1);
-		free(parsed_line_read);
+		}
+		else
+		{
+			ft_strlcpy(sh->input_string, parsed_line,
+				ft_strlen(parsed_line) + 1);
+			free(parsed_line);
+		}
+
 	}
 	else if (line_read == NULL)
 		eof_exit_shell(sh);
-
 	free(line_read);
 	return (SUCCESS);
 }
