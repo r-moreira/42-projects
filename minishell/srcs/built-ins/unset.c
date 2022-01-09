9f60c8fa96605fd867d9db310a51455ca9e1ddbb
r-moreira <r.moreira4297@gmail.com>
2022-01-09 18:47:03 -0300

    FEATURE: Adding unset built-in

--- minishell/srcs/built-ins/unset.c
+++ minishell/srcs/built-ins/unset.c
@@ -6,13 +6,13 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 23:19:07 by romoreir          #+#    #+#             */
-/*   Updated: 2022/01/09 17:21:14 by romoreir         ###   ########.fr       */
+/*   Updated: 2022/01/09 18:46:28 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-t_bool	is_valid_unset(char *env)
+static t_bool	is_valid_unset(char *env)
 {
 	int		i;
 
@@ -25,10 +25,43 @@ t_bool	is_valid_unset(char *env)
 	return (TRUE);
 }
 
-void	remove_env(t_shell *sh, char *env)
+
+void	remove_env(t_shell *sh, char *arg)
 {
-	(void)sh;
-	(void)env;
+	int		i;
+	int		j;
+	int		len;
+	char	*tmp[MAX_ENVS];
+	char	*env_key;
+
+	len = sh->count.envs;
+	i = -1;
+	while (++i < len)
+	{
+		tmp[i] = (char *)malloc(sizeof(char) * ft_strlen(sh->envs[i]) + 1);
+		ft_strlcpy(tmp[i], sh->envs[i], ft_strlen(sh->envs[i]) + 1);
+	}
+	tmp[i + 1] = NULL;
+	i = -1;
+	while (++i < len)
+		free(sh->envs[i]);
+	i = -1;
+	j = -1;
+	while (++i < len)
+	{
+		env_key = get_env_key(tmp[i]);
+		if (ft_strncmp(arg, env_key, ft_strlen(arg) + 1))
+		{
+			sh->envs[++j] = (char *)malloc(sizeof(char) * ft_strlen(tmp[i]) + 1);
+			ft_strlcpy(sh->envs[j], tmp[i], ft_strlen(tmp[i]) + 1);
+		}
+		else
+			sh->count.envs--;
+		free(env_key);
+	}
+	i = -1;
+	while (++i < len)
+		free(tmp[i]);
 }
 
 t_status	ft_unset(t_shell *sh, int cmd_num)
