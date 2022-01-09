97a07c8ff956d430dd4779a6bd4499c66596be53
r-moreira <r.moreira4297@gmail.com>
2022-01-09 00:13:33 -0300

    FEATURE: Adding init shell aux function

---
+++ minishell/srcs/utils/init_shell.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_shell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/08 23:23:35 by romoreir          #+#    #+#             */
+/*   Updated: 2022/01/09 00:10:34 by romoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+static void	setup_envs(t_shell *sh, char **envp)
+{
+	int	i;
+
+	i = -1;
+	while (envp[++i])
+	{
+		sh->envs[i] = (char *)malloc(sizeof(char) * ft_strlen(envp[i]) + 1);
+		ft_strlcpy(sh->envs[i], envp[i], ft_strlen(envp[i]) + 1);
+	}
+	sh->count.envs = i;
+}
+
+static void	setup_paths(t_shell *sh)
+{
+	char	*path_env;
+	char	**split;
+	int		i;
+
+	i = -1;
+	path_env = getenv("PATH");
+	if (path_env == NULL)
+		return ;
+	split = ft_split(path_env, ':');
+	while (split[++i])
+	{
+		sh->paths[i] = (char *)malloc(sizeof(char) * ft_strlen(split[i]) + 1);
+		ft_strlcpy(sh->paths[i], split[i], ft_strlen(split[i]) + 1);
+	}
+	sh->count.paths = i;
+	i = -1;
+	while (split[++i])
+		free(split[i]);
+	free(split);
+}
+
+void	init_shell(t_shell *sh, char **envp)
+{
+	setup_envs(sh, envp);
+	setup_paths(sh);
+}
