347d3f3188a98631fd7853757fe9aa95f0e6f4d3
r-moreira <r.moreira4297@gmail.com>
2022-01-09 00:15:59 -0300

    FEATURE: Adding init shell aux function

--- minishell/srcs/built-ins/env.c
+++ minishell/srcs/built-ins/env.c
@@ -6,15 +6,22 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 23:19:25 by romoreir          #+#    #+#             */
-/*   Updated: 2022/01/05 23:23:47 by romoreir         ###   ########.fr       */
+/*   Updated: 2022/01/09 00:15:58 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+
+//TO-DO
+//Adicionar tratamento de flags ao comando (> < | >> <<)
+//Adicionar in e out fd na struct dependendo da flag;
 t_status	ft_env(t_shell *sh)
 {
-	printf("Calling env...\n");
-	(void)sh;
+	int	i;
+
+	i = -1;
+	while (++i < sh->count.envs)
+		printf("%s\n", sh->envs[i]);
 	return (SUCCESS);
 }
