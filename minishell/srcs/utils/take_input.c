4457d9856d45fe28263c0b6b83ef332258f0994f
r-moreira <r.moreira4297@gmail.com>
2022-02-22 22:19:38 -0300

    Fixing heredoc with redirect out

--- minishell/srcs/utils/take_input.c
+++ minishell/srcs/utils/take_input.c
@@ -6,7 +6,7 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 21:10:24 by romoreir          #+#    #+#             */
-/*   Updated: 2022/02/12 21:58:50 by romoreir         ###   ########.fr       */
+/*   Updated: 2022/02/22 22:16:19 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,7 @@
 static t_status	heredoc_aux(t_shell *sh, char *parsed)
 {
 	char	*input_end;
-	char	*tmp;
 
-	tmp = NULL;
 	input_end = parse_heredoc_input_end(parsed);
 	if (input_end[0] == '\0')
 	{
@@ -26,9 +24,7 @@ static t_status	heredoc_aux(t_shell *sh, char *parsed)
 		return (syntax_error(ERROR_HEREDOC));
 	}
 	ft_strlcpy(sh->heredoc_input_end, input_end, HERE_DOCUMENT_INPUT_END_SIZE);
-	tmp = ft_remove_substr(parsed, input_end, ft_strlen(parsed));
-	ft_strlcpy(sh->input_string, tmp, ft_strlen(tmp) + 1);
-	free(tmp);
+	ft_strlcpy(sh->input_string, parsed, ft_strlen(parsed) + 1);
 	free(input_end);
 	return (SUCCESS);
 }
