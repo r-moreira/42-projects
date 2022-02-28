c495a7a390437f8e3332bc801b47ab0a90e7234e
r-moreira <r.moreira4297@gmail.com>
2022-02-27 23:35:01 -0300

    Adding error check to is_here_document function

--- minishell/srcs/utils/is_here_document.c
+++ minishell/srcs/utils/is_here_document.c
@@ -6,7 +6,7 @@
 /*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 12:16:19 by romoreir          #+#    #+#             */
-/*   Updated: 2022/01/17 12:16:29 by romoreir         ###   ########.fr       */
+/*   Updated: 2022/02/27 23:34:14 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ t_bool	is_here_document(char *parsed_line)
 {
 	int	i;
 
+	if (!parsed_line || ft_strlen(parsed_line) == 0)
+		return (FALSE);
 	i = 0;
 	while (parsed_line[++i])
 		if (parsed_line[i - 1] == '<' && parsed_line[i] == '<')
