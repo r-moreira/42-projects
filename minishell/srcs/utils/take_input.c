8285bc24488d84a8eb66982860583f8d07ea42c5
Rodrigo Moreira <r.moreira4297@gmail.com>
2022-03-04 02:44:59 +0100

    Handling empty pipe on input end

--- minishell/srcs/utils/take_input.c
+++ minishell/srcs/utils/take_input.c
@@ -3,14 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   take_input.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
+/*   By: romoreir <coder@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 21:10:24 by romoreir          #+#    #+#             */
-/*   Updated: 2022/03/02 23:22:45 by romoreir         ###   ########.fr       */
+/*   Updated: 2022/03/04 02:44:38 by romoreir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static t_bool	is_pipe_end(char *parsed)
+{
+	int		i;
+	char	last_char;
+
+	last_char = '\0';
+	i = -1;
+	while (parsed[++i])
+	{
+		if (!ft_isspace(parsed[i]))
+			last_char = parsed[i];
+	}
+	if (last_char == '|')
+		return (TRUE);
+	return (FALSE);
+}
+
+static void	handle_pipe_end(t_shell *sh, char *parsed)
+{
+	char	*new;
+	char	*pipe_arg;
+
+	pipe_arg = readline("> ");
+	if (pipe_arg && *pipe_arg)
+	{
+		new = ft_strjoin(parsed, pipe_arg);
+		ft_strlcpy(sh->input_string, new, ft_strlen(new) + 1);
+		free(new);
+	}
+	free(pipe_arg);
+}
 
 t_status	take_input(t_shell *sh)
 {
@@ -24,7 +56,10 @@ t_status	take_input(t_shell *sh)
 	{
 		add_history(line_read);
 		parsed = parse_env_variables(line_read);
-		ft_strlcpy(sh->input_string, parsed, ft_strlen(parsed) + 1);
+		if (is_pipe_end(parsed))
+			handle_pipe_end(sh, parsed);
+		else
+			ft_strlcpy(sh->input_string, parsed, ft_strlen(parsed) + 1);
 		free(parsed);
 	}
 	else if (line_read == NULL)
