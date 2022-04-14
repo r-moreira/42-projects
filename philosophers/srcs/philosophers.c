77844f93c7ecacf2a423edd3b7a00d89b12ea254
r-moreira <r.moreira4297@gmail.com>
2022-04-14 19:32:33 -0300

    Adding basic project structure

---
+++ philosophers/srcs/philosophers.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romoreir < romoreir@student.42sp.org.br    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 19:18:15 by romoreir          #+#    #+#             */
+/*   Updated: 2022/04/14 19:24:03 by romoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	printf("Hello World!");
+	return (EXIT_SUCCESS);
+}
