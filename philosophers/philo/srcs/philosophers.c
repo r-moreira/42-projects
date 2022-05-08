363e44891a7a938bb955c8c0886bfed7ba5333cd
Rodrigo Moreira <r.moreira4297@gmail.com>
2022-05-09 00:10:08 +0200

    Adding new src folder

---
+++ philosophers/philo/srcs/philosophers.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romoreir <coder@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 19:46:12 by rarodrig          #+#    #+#             */
+/*   Updated: 2022/05/08 23:56:32 by romoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+int	main(int argc, char **argv)
+{
+	t_control	control;
+	int			i;
+
+	if (!is_args_valid(argc, argv))
+		return (EXIT_FAILURE);
+	setup(&control, argc, argv);
+	pthread_mutex_init(&control.out, NULL);
+	i = -1;
+	while (++i < control.philosopher_number)
+		pthread_create(&control.philo[i].thread, NULL,
+			&philosophers_routine, &control.philo[i]);
+	pthread_create(&control.monitor, NULL, &monitor_routine, &control);
+	i = -1;
+	while (++i < control.philosopher_number)
+		pthread_join(control.philo[i].thread, NULL);
+	pthread_join(control.monitor, NULL);
+	free(control.philo);
+	free(control.forks);
+	return (EXIT_SUCCESS);
+}
