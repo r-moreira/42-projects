6eba84b4a028c5922ef829a5dfbc585852ae3a86
Rodrigo Moreira <r.moreira4297@gmail.com>
2022-05-13 04:56:47 +0200

    Updating main

--- cpp_modules/m02/ex00/srcs/main.cpp
+++ cpp_modules/m02/ex00/srcs/main.cpp
@@ -1,4 +1,25 @@
-int main() {
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romoreir <coder@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 05:05:37 by romoreir          #+#    #+#             */
+/*   Updated: 2022/05/10 05:05:37 by romoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    return (0);
+#include <iostream>
+
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	
+	cc = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
