977a84b204f2bbec29b8af70d2110f9b22ca0812
Rodrigo Moreira Avila <rodrigo.avila@telefonicabigdata.onmicrosoft.com>
2022-05-21 11:55:24 -0300

    Adding ex00 definitions

---
+++ cpp_modules/m04/ex00/srcs/Cat.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romoreir <coder@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 05:05:37 by romoreir          #+#    #+#             */
+/*   Updated: 2022/05/10 05:05:37 by romoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef M04_CAT_H
+#define M04_CAT_H
+
+#include "Animal.h"
+
+class Cat : public Animal {
+public:
+    Cat();
+
+    Cat(const Cat &cat);
+
+    ~Cat();
+
+    Cat &operator=(const Cat &cat);
+
+    void makeSound() const;
+};
+
+#endif //M04_CAT_H
---
+++ cpp_modules/m04/ex00/srcs/Dog.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romoreir <coder@student.42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/10 05:05:37 by romoreir          #+#    #+#             */
+/*   Updated: 2022/05/10 05:05:37 by romoreir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef M04_DOG_H
+#define M04_DOG_H
+
+#include "Animal.h"
+
+class Dog : public Animal {
+public:
+    Dog();
+
+    Dog(const Dog &dog);
+
+    ~Dog();
+
+    Dog &operator=(const Dog &dog);
+
+    void makeSound() const;
+};
+
+#endif //M04_DOG_H
