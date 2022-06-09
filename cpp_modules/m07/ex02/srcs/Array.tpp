a699e35facaf5c806c9656eb8ec6e773c387c8f2
Rodrigo Moreira Avila <r.moreira4297@gmail.com>
2022-06-09 00:02:47 -0300

    Addding cpp m07 ex02

---
+++ cpp_modules/m07/ex02/srcs/Array.tpp
@@ -0,0 +1,63 @@
+#ifndef M07_ARRAY_TPP
+#define M07_ARRAY_TPP
+
+#include <exception>
+#include <iostream>
+
+template<typename T>
+class Array {
+private:
+    unsigned int _n;
+    T *_data;
+
+public:
+    Array() : _n(0), _data(0) {};
+
+    Array(unsigned int n) : _n(n), _data(new T[n]()) {
+        for (unsigned int i = 0; i < n; i++) {
+            this->_data[i] = 0;
+        }
+    };
+
+    Array(Array const &array) : _n(array.size()), _data(new T[this->_n]()) {
+        *this = array;
+    };
+
+    ~Array() {
+        if (this->_data) {
+            delete[] this->_data;
+        }
+    }
+
+    Array &operator=(Array const &array) {
+        delete[] this->_data;
+
+        this->_data = new T[array._n];
+        this->_n = array._n;
+
+        for (unsigned int i = 0; i < array._n; i++)
+            this->_data[i] = array._data[i];
+
+        return *this;
+    }
+
+    T &operator[](unsigned int n) {
+        if (n >= _n || n < 0) {
+            throw IndexOutOfBoundsException();
+        }
+        return (_data[n]);
+    }
+
+    unsigned int size() const {
+        return (_n);
+    };
+
+    class IndexOutOfBoundsException : public std::exception {
+    public:
+        const char *what() const throw() {
+            return "Error: Array Overflow due to invalid memory access";
+        }
+    };
+};
+
+#endif //M07_ARRAY_TPP
--- cpp_modules/m07/ex02/srcs/main.cpp
+++ cpp_modules/m07/ex02/srcs/main.cpp
@@ -1,6 +1,92 @@
 #include <iostream>
+#include "Array.tpp"
+#include <cstdlib>
+#include <ctime>
 
-int main(int argc, char **argv) {
+void testArrayIndexOperatorValues(const int &len) {
+
+    Array<int> myArray(len);
+    int *cppArray = new int[len];
+
+    srand(time(NULL));
+    for (int i = 0; i < len; i++) {
+        const int value = rand();
+        myArray[i] = value;
+        cppArray[i] = value;
+    }
+
+    for (int i = 0; i < len; i++) {
+        if (myArray[i] != cppArray[i]) {
+            std::cerr << "Error: Arrays have different values for same index" << std::endl;
+            exit(1);
+        }
+    }
+
+    std::cout << "Success: Both arrays have same vales for the same index" << std::endl;
+}
+
+void testArrayException() {
+    Array<int> array(5);
+
+    try {
+        array[6] = 0;
+    } catch (const std::exception &e) {
+        std::cout << "Success: Array throw an exception" << std::endl;
+    }
+}
+
+void testSizeFunction() {
+    Array<char> array(42);
+
+    if (array.size() == 42) {
+        std::cout << "Success: Array has correct size" << std::endl;
+    } else {
+        std::cerr << "Error: Array has an invalid size" << std::endl;
+    }
+}
+
+void testChangeSpecficIndexValue() {
+    Array<float> array(3);
+
+    array[2] = 42.0f;
+
+    if (array[2] == 42.0f) {
+        std::cout << "Success: Array index has correct value" << std::endl;
+    } else {
+        std::cerr << "Error: Array has an incorrect value" << std::endl;
+    }
+}
+
+void testArrayCopy() {
+    Array<int> array = Array<int>(5);
+    Array<int> arrayCopy(array);;
+
+    array[2] = 42;
+    if (arrayCopy[2] != 42) {
+        std::cout << "Success: Array copy not changed" << std::endl;
+    } else {
+        std::cerr << "Error: Array has changed" << std::endl;
+    }
+}
+
+void testEmptyConstructor() {
+    Array<std::string> array = Array<std::string>();
+
+    if (array.size() == 0) {
+        std::cout << "Success: Empty constructor has initialized an empty array" << std::endl;
+    } else {
+        std::cerr << "Error: Empty constructor has not initialized an empty array" << std::endl;
+    }
+}
+
+
+int main(int, char **) {
+    testArrayIndexOperatorValues(1000);
+    testArrayException();
+    testSizeFunction();
+    testChangeSpecficIndexValue();
+    testArrayCopy();
+    testEmptyConstructor();
 
     return 0;
 }
