e38d88461237bee6e0d696448be4f5c9b7b29a7f
Rodrigo Moreira Avila <r.moreira4297@gmail.com>
2022-06-11 21:54:37 -0300

    Adding m08 ex00

---
+++ cpp_modules/m08/ex00/srcs/easyfind.h
@@ -0,0 +1,33 @@
+#ifndef M08_EASYFIND_H
+#define M08_EASYFIND_H
+
+#include <iostream>
+#include <algorithm>
+
+class OccurrenceNotFoundException : public std::exception {
+public:
+    virtual const char *what() const throw() {
+        return "No occurrence is found";
+    }
+};
+
+template<typename T>
+void easyfind(T &t, int n) {
+    typename T::const_iterator iter;
+
+    iter = std::find(t.begin(), t.end(), n);
+    try {
+        if (iter == t.end()) {
+            throw (OccurrenceNotFoundException());
+        }
+    }
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+        return;
+    }
+
+    std::cout << "Element occurrence found: " << *iter << std::endl;
+}
+
+
+#endif //M08_EASYFIND_H
--- cpp_modules/m08/ex00/srcs/main.cpp
+++ cpp_modules/m08/ex00/srcs/main.cpp
@@ -1,3 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include "easyfind.h"
+
+template<typename T>
+void testOccurrence(T t) {
+    for (int i = 0; i <= 42; i++) {
+        t.push_back(i);
+    }
+
+    easyfind(t, 21);
+    easyfind(t, 42);
+    easyfind(t, 84);
+}
+
 int main() {
-	return 0;
+    std::cout << "===== Vector Tests =====" << std::endl;
+    std::vector<int> v;
+    testOccurrence(v);
+
+    std::cout << "===== List Tests =====" << std::endl;
+    std::list<int> l;
+    testOccurrence(l);
+
+    return 0;
 }
