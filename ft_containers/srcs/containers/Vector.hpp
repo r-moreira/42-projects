f50440b8fbb60697d92bb85ade42a24bdf909add
Rodrigo Avila <r.moreira4297@gmail.com>
2022-07-24 00:12:34 -0300

    Starting vector tests

--- ft_containers/srcs/containers/Vector.hpp
+++ ft_containers/srcs/containers/Vector.hpp
@@ -7,9 +7,10 @@
 namespace ft {
 
     template<class T, class Alloc = std::allocator<T> >
+
     class vector {
 
-    public:
+        ////////////////////// TYPEDEFS //////////////////////
         typedef T												value_type;
         typedef	Alloc											allocator_type;
         typedef value_type&										reference;
@@ -18,10 +19,13 @@ namespace ft {
         typedef const value_type*								const_pointer;
         typedef ptrdiff_t										difference_type;
         typedef size_t											size_type;
-//        typedef RandomAccessIterator<T, T*, T&>					iterator;
-//        typedef RandomAccessIterator<T, const T*, const T&>		const_iterator;
-//        typedef ReverseRAI<iterator>							reverse_iterator;
-//        typedef ReverseRAI<const_iterator >						const_reverse_iterator;
+
+        /* TODO: Random Access Iterators for Member Types
+        typedef RandomAccessIterator<T, T*, T&>					iterator;
+        typedef RandomAccessIterator<T, const T*, const T&>		const_iterator;
+        typedef ReverseRandomAccessIterator<iterator>			reverse_iterator;
+        typedef ReverseRandomAccessIterator<const_iterator >	const_reverse_iterator;
+        */
 
     /* Non-member function overloads */
     private:
@@ -31,9 +35,54 @@ namespace ft {
         allocator_type	_alloc;
 
     public:
-        explicit vector (const allocator_type& alloc = allocator_type()) : _array(0), _size(0), _capacity(0), _alloc(alloc) {
+
+        ////////////////////// CONSTRUCTORS - RULE OF THREE //////////////////////
+        explicit vector(const allocator_type& alloc = allocator_type()) : _array(0), _size(0), _capacity(0), _alloc(alloc) {}
+
+        explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _size(n), _capacity(n), _alloc(alloc) {
+            this->_array = new value_type[n]();
+            for (size_type i = 0; i < n; i++) this->_array[i] = val;
         }
 
+        vector(const vector& v) : _array(0), _size(0), _capacity(0), _alloc(v._alloc) { *this = v; }
+
+        // TODO: Create Constructor for Iterators
+
+        /* TODO: Waiting for Iterators and Modifiers
+        vector&	operator=(const vector& v) {
+            delete[] this->_array;
+            this->_array = new value_type[v._capacity];
+            this->_capacity = v._capacity;
+            this->_size = 0;
+            this->assign(v.begin(), v.end());
+            return *this;
+        }
+        */
+
+        ~vector() { delete[] this->_array; }
+
+        ////////////////////// ELEMENT ACCESS //////////////////////
+        reference operator[](size_type n) { return this->_array[n]; }
+
+        const_reference	operator[](size_type n) const { return this->_array[n]; }
+
+        reference at(size_type n) {
+            if (n >= this->_size) throw std::out_of_range("out of range");
+            return this->_array[n];
+        }
+
+        const_reference	at(size_type n) const {
+            if (n >= this->_size) throw std::out_of_range("out of range");
+            return this->_array[n];
+        }
+
+        reference front() { return this->_array[0]; }
+
+        const_reference	front() const { return this->_array[0]; }
+
+        reference back() { return this->_array[this->_size - 1]; }
+
+        const_reference	back() const { return this->_array[this->_size - 1]; }
     };
 }
 
--- ft_containers/srcs/tests/main.cpp
+++ ft_containers/srcs/tests/main.cpp
@@ -1,11 +1,72 @@
 #include <iostream>
 #include "../containers/Vector.hpp"
 #include <vector>
+#include <cstdlib>
+
+void TEST(const std::string& desc) {
+    std::cout << "\033[1;34m#################### "<<desc<<" ####################"<<"\033[0m\n";
+}
+
+void TODO() { std::cout << "\033[1;33mTODO\033[0m\n"; }
+
+void OK() { std::cout << "\033[1;32mOK\033[0m\n"; }
+
+void ERR() { std::cout << "\033[1;31mERROR\033[0m\n";}
+
+template<typename T, typename  V> void
+ERR(T std, V ft) {
+    std::cout << "\033[1;31mERROR\033[0m" << std::endl;
+    std::cout << "\tft = " << ft <<  std::endl;
+    std::cout << "\tstd: " << std << std::endl;
+}
+
+int randomIndex(int max) {
+    srand(time(NULL));
+    return rand() % max;
+}
+
+void vector_constructor_tests() {
+    TEST("VECTOR CONSTRUCTOR TESTS");
+
+    int i = 0;
+
+    std::cout << "> Empty Constructor: ";
+    ft::vector<double> ftEmpty;
+    std::vector<double> stdEmpty;
+    OK();
+
+    std::cout << "> Fill Constructor with default value: ";
+    ft::vector<double> ft(21);
+    std::vector<double> std(21);
+    i =  randomIndex(21);
+    ft[i] == std[i] ? OK() : ERR(ft[i], std[i]);
+
+    std::cout << "> Fill Constructor with specific value (42): ";
+    ft::vector<int> ft2(21, 42);
+    std::vector<int> std2(21, 42);
+    i =  randomIndex(21);
+    ft2[i] == std2[i] ? OK() : ERR(ft2[i], std2[i]);
+
+    std::cout << "> Iterator Constructor Test: ";
+    TODO();
+
+    std::cout << "> Copy Constructor Test: ";
+    TODO();
+}
+
+void vector_element_access_tests() {
+    TEST("VECTOR ELEMENT ACCESS TESTS");
+    TODO();
+}
+
+void vector_operators_tests() {
+    TEST("VECTOR OPERATORS TESTS");
+    TODO();
+}
 
 int main() {
-    std::vector<int> stdVector;
-    ft::vector<int> myVector;
-
-    std::cout << "It works!!" << std::endl;
+    vector_constructor_tests();
+    vector_operators_tests();
+    vector_element_access_tests();
     return 0;
 }
