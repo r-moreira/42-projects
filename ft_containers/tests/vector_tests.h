62e21398f1169ef8ebffac6243000db497eee2b8
Rodrigo Avila <r.moreira4297@gmail.com>
2022-08-14 19:04:46 -0300

    Refacotring vector tests

---
+++ ft_containers/tests/vector_tests.h
@@ -0,0 +1,11 @@
+#ifndef FT_CONTAINERS_VECTOR_TESTS_H
+#define FT_CONTAINERS_VECTOR_TESTS_H
+
+void vector_constructor_tests();
+void vector_element_access_tests();
+void vector_capacity_tests();
+void vector_modifiers_tests();
+void vector_non_member_fcnt_overloads_tests();
+void vector_complex_types_tests();
+void vector_performance_tests();
+#endif //FT_CONTAINERS_VECTOR_TESTS_H
